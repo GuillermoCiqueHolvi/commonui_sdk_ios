8f80fb0328a4a595ec25f8ef50e5fe709058d0e9
Guillermo Cique <guillermo@holvi.com>
2020-11-16 16:21:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,10 +8,6 @@ let package = Package(
         .library(name: "CommonUISDK", targets: ["CommonUISDK"])
     ],
     targets: [
-        .binaryTarget(
-            name: "CommonUISDK",
-            url: "https://zendesk.jfrog.io/zendesk/libs-releases-local/ios/zendesk/5.2.4/CommonUISDK/6.0.0/CommonUISDK.zip",
-            checksum: "10f35f613359a79b6346704cfc2b281a225bf5a4a589e56a7bbfd1b763e3e073"
-        )
+        .binaryTarget(name: "CommonUISDK", path: "CommonUISDK.xcframework")
     ]
 )
